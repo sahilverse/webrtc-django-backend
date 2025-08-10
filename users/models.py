e97fdb28e8bab1fba5c552c6348827c07bc5cc14
Sahil Dahal <denenme49@gmail.com>
2025-08-10 11:20:41 +0545

    Refactor: Move image processing imports to the top of models.py for better organization

--- users/models.py
+++ users/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from django.core.files.base import ContentFile
+
+from PIL import Image
+from io import BytesIO
 
 from .managers import CustomUserManager
 
