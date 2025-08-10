db70d9cf38831008cc611d3da37e471b2f23cc05
Sahil Dahal <denenme49@gmail.com>
2025-08-10 15:57:57 +0545

    Feat: Add media file settings for handling uploads in the project

--- myproject/settings.py
+++ myproject/settings.py
@@ -157,6 +157,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -220,6 +224,3 @@ CHANNEL_LAYERS = {
 }
 
 
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
