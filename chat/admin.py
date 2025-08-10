9985919ce59e4a510169f0c03015b8d442a2756a
Sahil Dahal <denenme49@gmail.com>
2025-08-10 12:35:35 +0545

    Refactor: Organize model registrations in admin.py for improved clarity

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+import chat.models
+
 # Register your models here.
+admin.site.register(chat.models.Chat)
+admin.site.register(chat.models.ChatMember)
+admin.site.register(chat.models.Message)
+admin.site.register(chat.models.MessageReaction)
+admin.site.register(chat.models.MessageStatusEntry)
+admin.site.register(chat.models.Call)
+admin.site.register(chat.models.CallParticipant)
